dcb51c811328e9b3b43baa3a2ea4ad41bc45137b
sabriAla <sabrinaalanoca@gmail.com>
2026-09-25 14:39:06 -0300

    configuracion de seguridad en alumno y arreglo de swagger

--- Alumno/src/main/java/com/app/Alumno/AlumnoApplication.java
+++ Alumno/src/main/java/com/app/Alumno/AlumnoApplication.java
@@ -2,12 +2,17 @@ package com.app.Alumno;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { 
+    SecurityAutoConfiguration.class, 
+    ManagementWebSecurityAutoConfiguration.class 
+})
 public class AlumnoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlumnoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AlumnoApplication.class, args);
+    }
 
 }
